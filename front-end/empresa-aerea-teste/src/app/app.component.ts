fae88691bf6ca58eac01083f62bc621fbc8b2ad6
Laísa Cristina Krolikovski da Silva <laisacristina@gmail.com>
2024-09-15 17:00:07 -0300

    Criacao do projeto angular, pagina home-cliente e componente navbar

---
+++ front-end/empresa-aerea-teste/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeClienteComponent } from './paginas/home-cliente/home-cliente.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HomeClienteComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'empresa-aerea-teste';
+}
---
+++ front-end/empresa-aerea-teste/src/app/paginas/home-cliente/home-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-cliente.component.html',
+  styleUrl: './home-cliente.component.css'
+})
+export class HomeClienteComponent {
+
+}
