b1ed392610adae14238ed9ec42c226cb2ff6583f
Victoria <vkvieira@yahoo.com>
2024-09-18 21:51:59 -0300

    :broom: cleanup: removendo pedaço de código comentado

--- front-end/empresa-aerea-teste/src/app/app-routing.module.ts
+++ front-end/empresa-aerea-teste/src/app/app-routing.module.ts
@@ -2,17 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeClienteComponent } from './paginas/cliente/home-cliente/home-cliente.component';
 
-// const routes: Routes = [
-//   { path: '', component: HomeClienteComponent },
-//   // mais rotas
-// ];
+
 const routes: Routes = [
   // Passando o clienteId como parte da rota
   { path: 'cliente/:id', component: HomeClienteComponent },
   
-  // // rota inicial ou outras rotas
-  { path: '', redirectTo: '/cliente/1', pathMatch: 'full' }, // Exemplo de redirecionamento para o clienteId 1
-  // mais rotas
 ];
 
 @NgModule({
