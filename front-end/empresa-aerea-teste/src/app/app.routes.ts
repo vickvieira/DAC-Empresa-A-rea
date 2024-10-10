6eefc0b6c2edbf7a428fecc9374ba00699df9d35
Eduardo <edubello93@gmail.com>
2024-10-10 20:33:31 -0300

    r05 iniciado

--- front-end/empresa-aerea-teste/src/app/app.routes.ts
+++ front-end/empresa-aerea-teste/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeClienteComponent } from './paginas/cliente/home-cliente/home-cliente.component';
 import { AutCadComponent } from './paginas/aut-cad/aut-cad.component';
+import { ComprarMilhasComponent } from './paginas/cliente/comprar-milhas/comprar-milhas.component';
 
 export const routes: Routes = [
     { path: 'cliente/:id', component: HomeClienteComponent },
-    { path: 'cadastrar', component: AutCadComponent}
+    { path: 'cadastrar', component: AutCadComponent},
+    { path: 'cliente/:id/comprar-milhas', component: ComprarMilhasComponent}
 ];
---
+++ front-end/empresa-aerea-teste/src/app/paginas/cliente/comprar-milhas/comprar-milhas.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MilhasService } from '../../../services/milhas.service';
+
+
+
+@Component({
+  selector: 'app-comprar-milhas',
+  standalone: true,
+  imports: [],
+  templateUrl: './comprar-milhas.component.html',
+  styleUrl: './comprar-milhas.component.css'
+})
+export class ComprarMilhasComponent {
+
+}
