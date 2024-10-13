0e349caa258a84e9a07acb2506d930102ed61357
Matheus Dionysio <142601649+didionysio@users.noreply.github.com>
2024-10-13 20:19:47 -0300

    R02Matheus  Criada tela de Login e func de Login no service

--- front-end/empresa-aerea-teste/src/app/paginas/login/login/login.component.ts
+++ front-end/empresa-aerea-teste/src/app/paginas/login/login/login.component.ts
@@ -1,26 +1,45 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginService } from '../../../services/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
+
 export class LoginComponent {
-  clienteId: number | null = null;
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required ]]
+    });
+  }
 
-  login(): void {
-    if (this.clienteId !== null) {
-      this.authService.login(this.clienteId);
-      this.router.navigate(['/cliente']); // Redirecionar para a página principal do cliente após o login
-    } else {
-      alert('Por favor, insira um ID de cliente válido.');
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, senha } = this.loginForm.value;
+
+      this.loginService.login(email, senha).subscribe(
+        response => {
+          if (response.success) {
+            // Se a senha estiver correta, redireciona para outra página ou realiza uma ação
+            console.log('Login bem-sucedido');
+          } else {
+            this.errorMessage = 'Credenciais inválidas. Tente novamente.';
+          }
+        },
+        error => {
+          this.errorMessage = 'Ocorreu um erro. Tente novamente mais tarde.';
+        }
+      );
     }
   }
 }
