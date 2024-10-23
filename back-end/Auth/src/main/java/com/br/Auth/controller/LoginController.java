d4fb24bc8cdd2b7f75993ea41989e5930d9778c8
Matheus Dionysio <142601649+didionysio@users.noreply.github.com>
2024-10-22 23:28:34 -0300

    Código com problema comentado

--- back-end/Auth/src/main/java/com/br/Auth/controller/LoginController.java
+++ back-end/Auth/src/main/java/com/br/Auth/controller/LoginController.java
@@ -15,13 +15,13 @@ public class LoginController {
     @Autowired
     private LoginREST usuarioService;
 
-    @GetMapping
-    public List<Login> getAllUsuarios() {
-        return usuarioService.getAllUsuarios();
-    }
-
-    @PostMapping
-    public Login createUsuario(@RequestBody Login usuarioDTO) {
-        return usuarioService.saveUsuario(usuarioDTO);
-    }
+//    @GetMapping
+//    public List<Login> getAllUsuarios() {
+//        return usuarioService.getAllUsuarios();
+//    }
+//
+//    @PostMapping
+//    public Login createUsuario(@RequestBody Login usuarioDTO) {
+//        return usuarioService.saveUsuario(usuarioDTO);
+//    }
 }
