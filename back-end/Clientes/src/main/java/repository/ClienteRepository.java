392dfb6564aa3647fbd63fe4802babc83ec31912
Matheus Dionysio <142601649+didionysio@users.noreply.github.com>
2024-12-10 22:24:20 -0300

    correção de pacotes

--- back-end/Clientes/src/main/java/repository/ClienteRepository.java
+++ back-end/Clientes/src/main/java/repository/ClienteRepository.java
@@ -3,7 +3,7 @@ package repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import DTO.ClientesDTO;
+import dto.ClientesDTO;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<ClientesDTO, Long> {
